cc53cad1ef58cad5f5b92519b19f824c666a5b65
Pablo Morales <pablocoder01@gmail.com>
2023-07-11 02:37:46 -0300

    3ra PreEntrega para Coder

---
+++ main.js
@@ -0,0 +1,60 @@
+const navItems = document.querySelector('#nav__items');
+const openNavBtn = document.querySelector('#open__nav-btn');
+const closeNavBtn = document.querySelector('#close__nav-btn');
+
+openNavBtn.addEventListener('click', () => {
+    navItems.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+})
+
+
+const closeNav = () => {
+    navItems.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+}
+
+closeNavBtn.addEventListener('click', closeNav);
+
+
+// Close nav menu when menu item is clicked
+if(window.innerWidth < 1024) {
+    document.querySelectorAll('#nav__items li a').forEach(navItem => {
+        navItem.addEventListener('click', () => {
+            closeNav();
+        })
+    })
+}
+
+
+
+// Change navbar styles on scroll
+window-addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+})
+
+
+
+
+// Testimonials Section (Swiper js)
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    // Responsive Breakpoints
+    breakpoints: {
+        // when window with is >= 600px
+        600: {
+            slidesPerView: 2
+        },
+    // when window with is >= 600px
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
